e14ae30d3dc0469d13bb95feefa4ebc2f5e0de35
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-07-02 21:04:32 +0900

    terraform test

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals{
+  location = "japaneast"
+  resource_group_name = "example-resources"
+  vnet_name = "example-network"
+  public_subnet_name = "subnet1"
+  private_subnet_name = "subnet2"
+  nic_name="example-nic"
+}
---
+++ nic.tf
@@ -0,0 +1,11 @@
+resource "azurerm_network_interface" "nic" {
+  name                = local.nic_name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.private.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
