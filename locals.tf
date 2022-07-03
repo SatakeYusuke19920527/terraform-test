dfd7a1b356e1c50fb2a1980008db78ce0b3c30bb
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-07-03 09:32:21 +0900

    commit

--- locals.tf
+++ locals.tf
@@ -4,5 +4,8 @@ locals{
   vnet_name = "example-network"
   public_subnet_name = "subnet1"
   private_subnet_name = "subnet2"
-  nic_name="example-nic"
+  nic_private_name="example-private-nic"
+  nic_public_name="example-public-nic"
+  nsg_public_name="example-public-nsg"
+  nsg_private_name="example-private-nsg"
 }
